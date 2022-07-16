ae05311f172a494c2afaa4fee4830bb305339ded
Bharat Goyal <bgoyal1471@gmail.com>
2022-07-17 02:57:57 +0530

    Style: header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Header } from "./components";
+import { Header, MainContainer } from "./components";
+import CreateContainer from "./components/CreateContainer";
+import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex-col ">
-      <Header />
-    </div>
+    <AnimatePresence>
+      <div className="w-screen h-auto flex-col bg-primary">
+        <Header />
+        <main className="mt-0 p-28 w-full">
+          <Routes>
+            <Route path="/" element={MainContainer} />
+            <Route path="/CreateItem" element={CreateContainer} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Logo from "./img/logo.png";
+import Avatar from "./img/avatar.png";
+import { motion } from "framer-motion";
 import { MdShoppingBag } from "react-icons/md";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <header className="fixed z-50 w-screen  p-6 px-16">
+    <header className="fixed z-50 w-screen  p-6 px-14">
       {/* desktop and tablet  */}
 
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <div className="flex items-center gap-2">
+        <Link to = {'/'} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold">foodbyBharat</p>
-        </div>
+        </Link>
 
         <div className="flex items-center gap-8">
           <ul className="flex items-center gap-8 ml-auto">
@@ -30,12 +32,18 @@ const Header = () => {
           </ul>
 
           <div className="relative flex items-center justify-center">
-            <MdShoppingBag className="text-textColor text-2xl ml-8 cursor-pointer" />
+            <MdShoppingBag className="text-textColor text-2xl ml-auto cursor-pointer" />
             <div className=" absolute -top-2 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-xs text-white font-semibold">2</p>
             </div>
           </div>
-          
+
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            src={Avatar}
+            className=" w-10 min-w-[40-px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
+            alt="userprofile"
+          />
         </div>
       </div>
       {/* mobile  */}
