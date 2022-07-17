aa82a663d4f26a20e7575ddd00b7464f4cb429bd
Bharat Goyal <bgoyal1471@gmail.com>
2022-07-18 02:15:39 +0530

    Refactor: userAuthentication_dropmenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./img/logo.png";
 import Avatar from "./img/avatar.png";
 import { motion } from "framer-motion";
@@ -14,6 +14,8 @@ const Header = () => {
   const provider = new GoogleAuthProvider();
   const [{ user }, dispatch] = useStateValue();
 
+  const [isMenu, setisMenu] = useState(false);
+
   const login = async () => {
     if (!user) {
       const {
@@ -25,6 +27,8 @@ const Header = () => {
       });
 
       localStorage.setItem("user", JSON.stringify(providerData[0]));
+    } else {
+      setisMenu(!isMenu);
     }
   };
   return (
@@ -68,16 +72,27 @@ const Header = () => {
               alt="userprofile"
               onClick={login}
             />
-            <div className="w-40 bg-gray-50 shadow-xl rounded-lg  flex  flex-col absolute top-12 right-0 px-4 py-2">
-              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text textColor text-base">
-                New Item
-                <MdAdd />
-              </p>
-              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text textColor text-base">
-                Logout
-                <MdLogout />
-              </p>
-            </div>
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1}}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg  flex  flex-col absolute top-12 right-0 "
+              >
+                {user && user.email === "bgoyal1471@gmail.com" && (
+                  <Link to={"/createItems"}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text textColor text-base">
+                      New Item
+                      <MdAdd />
+                    </p>
+                  </Link>
+                )}
+                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text textColor text-base">
+                  Logout
+                  <MdLogout />
+                </p>
+              </motion.div>
+            )}
           </div>
         </div>
       </div>
