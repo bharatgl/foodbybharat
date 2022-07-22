ebbdce7a93f57cfc6ded975bffcb2ae1703860ce
Bharat Goyal <bgoyal1471@gmail.com>
2022-07-23 01:39:02 +0530

    feat: Main_ui

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const App = () => {
     <AnimatePresence exitBeforeEnter>
       <div className="w-screen h-auto flex-col bg-primary">
         <Header />
-        <main className="mt-0 p-28 w-full">
+        <main className="mt-4 md:mt-0 px-8 md:px-16 py-4 w-full">
           <Routes>
-            <Route path="/" element={MainContainer} />
-            <Route path="/CreateItem" element={CreateContainer} />
+            <Route path="/" element={<MainContainer />} />
+            <Route path="/CreateItem" element={<CreateContainer />} />
           </Routes>
         </main>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Logo from "./img/logo.png";
-import Avatar from "./img/avatar.png";
+import Logo from "../img/logo.png";
+import Avatar from "../img/avatar.png";
 import { motion } from "framer-motion";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
@@ -42,7 +42,7 @@ const Header = () => {
     });
   };
   return (
-    <header className="fixed z-50 w-screen  p-3 px-4 md:p-6 md:px-16">
+    <header className="z-50 w-screen  p-4 px-4 md:p-6 md:px-16">
       {/* desktop and tablet  */}
 
       <div className="hidden md:flex w-full h-full items-center justify-between">
@@ -73,7 +73,7 @@ const Header = () => {
           </motion.ul>
 
           <div className="relative flex items-center justify-center">
-            <MdShoppingBag className="text-textColor text-2xl ml-auto cursor-pointer" />
+            <MdShoppingBag className="text-textColor text-2xl  cursor-pointer" />
             <div className=" absolute -top-2 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-xs text-white font-semibold">2</p>
             </div>
@@ -96,15 +96,15 @@ const Header = () => {
               >
                 {user && user.email === "bgoyal1471@gmail.com" && (
                   <Link to={"/createItems"}>
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text textColor text-base">
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer  hover:bg-slate-100 transition-all duration-100 ease-in-out text textColor text-base">
                       New Item
                       <MdAdd />
                     </p>
                   </Link>
                 )}
                 <p
-                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text textColor text-base
-                 onClick={logout}"
+                  className="m-2 p-2 rounded-md shadow-md flex items-center justify-center gap-3 cursor-pointer  bg-gray-200  hover:bg-gray-300 transition-all duration-100 ease-in-out text textColor text-base"
+                  onClick={logout}
                 >
                   Logout
                   <MdLogout />
