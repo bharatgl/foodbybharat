ba9f0583e608179dcb91da468a8d43a09df179f0
Bharat Goyal <bgoyal1471@gmail.com>
2022-07-23 05:32:15 +0530

    feat: changed_main_UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
     });
   };
   return (
-    <header className="z-50 w-screen  p-4 px-4 md:p-6 md:px-16">
+    <header className=" z-50 w-screen p-4 Wpx-6 md:p-6 md:px-16 bg-primary">
       {/* desktop and tablet  */}
 
       <div className="hidden md:flex w-full h-full items-center justify-between">
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Delivery from "../img/delivery.png";
 import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/data";
 
 const HomeContainer = () => {
   return (
@@ -8,7 +9,7 @@ const HomeContainer = () => {
       id="home"
       className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full "
     >
-      <div className=" py-2 flex-1 flex flex-col items-start md:items-center justify-center gap-6">
+      <div className=" py-2 flex-1 flex flex-col items-start md:items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-2 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
             Bike Delivery
@@ -40,13 +41,37 @@ const HomeContainer = () => {
           Order Now
         </button>
       </div>
-      <div className="py-2 flex-1 ">
+      <div className="py-2 flex-1 flex  items-center relative">
         <img
           src={HeroBg}
-          className="ml-auto h-420 w-full lg:w-auto lg:h-650"
+          className="ml-auto h-full w-full  lg:w-auto lg:h-650 "
           alt="hero-bg"
         ></img>
-        <div className="w-full h-full absolute flex items-center justify-center"></div>
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center  py-4 gap-4 flex-wrap ">
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className=" lg:w-190  p-4 bg-cardOverlay backdrop-blur-md  rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+              >
+                <img
+                  src={n.imageSrc}
+                  className="w-20 -mt-10 lg:w-40 lg:-mt-20"
+                  alt="I1"
+                />
+                <p className="text-base  lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {n.name}
+                </p>
+
+                <p className="text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.decp}
+                </p>
+                <p className="text-[12px] lg:text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">₹</span> {n.price}
+                </p>
+              </div>
+            ))}
+        </div>
       </div>
     </section>
   );
