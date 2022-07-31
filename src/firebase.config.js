015be68930b2c1734c2350856c7ea66508d4fa98
Bharat Goyal <bgoyal1471@gmail.com>
2022-08-01 04:36:59 +0530

    feat : Upload_data

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -15,7 +15,8 @@ const firebaseConfig = {
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
-const firestore = getFirestore(app);
+const firestore = getStorage(app);
 const storage = getStorage(app);
+// const storage = getFirestore.storage();
 
 export { app, firestore, storage };
